fdb74ac2bed7bcdfd40351cab4ade9f3b3a11995
jhelou <joeyhellou@gmail.com>
2018-05-21 22:06:31 +0200

    Fixed markdown XSS vulnerability

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -43,7 +43,7 @@ export function markedOptionsFactory(): MarkedOptions {
         tables: true,
         breaks: false,
         pedantic: false,
-        sanitize: false,
+        sanitize: true,
         smartLists: true,
         smartypants: false,
     };
